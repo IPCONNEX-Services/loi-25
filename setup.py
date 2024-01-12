d96e5a939395ede9c1c1ad4a12d7b91b56aa7900
yacineghoulam <yacineghoulam@gmail.com>
2024-01-12 20:19:51 +0100

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable 
-from module_loi25 import __version__ as version
+from ipconnex_module_loi25 import __version__ as version
 
 setup(
 	name='ipconnex_module_loi25',
